a045cbcc6a7b6c140e24643d59c65a42cd8f2532
AJ ROA <armandjohnroa@gmail.comgit config --global user.name AJ>
2026-06-24 03:03:49 +1000

    figuring out dictionary keys

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import statsapi
 
 ORIOLES_TEAM_ID = 110
+
+recentGameID = statsapi.last_game(ORIOLES_TEAM_ID)
+
+print(recentGameID)
+
+box_data = statsapi.boxscore_data(recentGameID)
+
+
+print(box_data.keys())
+
+print(box_data.get("home").get("team"))
