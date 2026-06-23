f4a40a7f2049e977849eb10f9a8fb29f738c284b
AJ ROA <armandjohnroa@gmail.comgit config --global user.name AJ>
2026-06-24 03:18:29 +1000

    beggining creation of checkifhome function

--- app.py
+++ app.py
@@ -2,13 +2,28 @@ import statsapi
 
 ORIOLES_TEAM_ID = 110
 
-recentGameID = statsapi.last_game(ORIOLES_TEAM_ID)
+def CheckIfHome(gameData :dict, teamID: int) -> bool:
+    box_data
 
-print(recentGameID)
+    print(homeTeam)
 
-box_data = statsapi.boxscore_data(recentGameID)
+    return false
 
 
-print(box_data.keys())
+def main():
+    recentGameID = statsapi.last_game(ORIOLES_TEAM_ID)
 
-print(box_data.get("home").get("team"))
+    box_data = statsapi.boxscore_data(recentGameID)
+
+    isHome = CheckIfHome(box_data, ORIOLES_TEAM_ID)
+
+    # print(recentGameID)
+
+
+    # print(box_data.keys())
+
+    # print(box_data.get("home").get("team"))
+
+if __name__ == "__main__":
+    main()
+
