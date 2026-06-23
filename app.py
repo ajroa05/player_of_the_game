e455a45d9112625fe3a3bc2148223f3d98b205f0
AJ ROA <armandjohnroa@gmail.comgit config --global user.name AJ>
2026-06-24 02:44:07 +1000

    initial commit

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-import mlbstatsapi
-from datetime import datetime
+import statsapi
 
-mlbAPI = mlbstatsapi.Mlb()
-
-team_id = mlbAPI.get_team_id("Baltimore Orioles")
-print(team_id)
-# todays_date = datetime.today()
-# print(todays_date)
-
-# todays_game = mlbAPI.get_game_ids(str(todays_date))
-
-# print(todays_game)
-
+ORIOLES_TEAM_ID = 110
